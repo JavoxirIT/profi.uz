93c8f0869b3f174e0bf9ce02f016446d70dccf46
Javoxir <javoxr5588@gmail.com>
2023-07-25 18:24:07 +0500

    25.07

--- src/pages/agreements.js
+++ src/pages/agreements.js
@@ -1,20 +1,16 @@
 import React, {useEffect, useState} from 'react';
 import {postFetch} from "../request/Fetch";
 import PageWrapperAuthorization from "../components/PageWrapperAuthorization";
-
 import parse from 'html-react-parser';
-
+import Link from "next/link";
 import {Typography} from "antd";
+import css from "../styles/Agreements.module.css"
+import style from "../styles/Authorization.module.css";
 
 const {Title} = Typography
 
 function Agreements({t, lang}) {
 
-	const styles = {
-		width: "100%",
-		maxWidth: "calc(100vw - 400px)",
-		paddingTop: 20
-	}
 
 	const [uzDesc, setUzDesc] = useState('');
 	const [ruDesc, setRuDesc] = useState('');
@@ -42,10 +38,29 @@ function Agreements({t, lang}) {
 	return (
 		<PageWrapperAuthorization title={t.agreemetText}>
 
-			<div style={styles} >
+			<div className={css.agreementsBlock}>
 				<Title level={3}>{t.agreemetText}</Title>
-				<div>
+				<div className={css.agreementsBlockText}>
 					{lang === "uz" ? parse(uzDesc) : parse(ruDesc)}
+				</div>
+				<div className={style.AuthorizationLinkBlock}>
+					<Link href={"/"}>
+                <span className={style.AuthorizationLinkSpan}>
+                  {t.linkHome}
+                </span>
+					</Link>
+				</div>
+				<div className={style.AuthorizationLinkBlock}>
+					<Link href={"/registration"}>
+						{" "}
+						<span className={style.AuthorizationLinkSpan}>
+                  {t.linkRegistration}
+                </span>{" "}
+					</Link>
+				</div>
+				<div className={style.AuthorizationLinkBlock}>
+					<Link href={"/authorization"}><span
+						className={style.AuthorizationLinkSpan}>{t.pageTitleAuth}</span></Link>
 				</div>
 			</div>
 		</PageWrapperAuthorization>
